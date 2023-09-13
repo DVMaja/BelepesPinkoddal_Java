4e486506be112afd63c07cb12b852621e2857f1f
DVMaja <majadreilinger@gmail.com>
2023-09-13 12:22:52 +0200

    alapszerekezet

---
+++ src/belepeskoddal/BelepesKoddal.java
@@ -0,0 +1,18 @@
+
+package belepeskoddal;
+
+public class BelepesKoddal {
+
+    public static void main(String[] args) {
+        KodBekeres();
+        KodEllenorzes();
+        
+    }
+
+    private static void KodBekeres() {
+    }
+
+    private static void KodEllenorzes() {
+    }
+    
+}
