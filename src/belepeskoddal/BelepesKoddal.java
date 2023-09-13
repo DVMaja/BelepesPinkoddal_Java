b013c9b7d0fa534d9c8a96b57d0253c351ba64a4
DVMaja <majadreilinger@gmail.com>
2023-09-13 12:36:32 +0200

    mködö pin bekérés és 3 esély a kód megadására OK

--- src/belepeskoddal/BelepesKoddal.java
+++ src/belepeskoddal/BelepesKoddal.java
@@ -1,18 +1,42 @@
-
 package belepeskoddal;
+
+import java.util.Scanner;
 
 public class BelepesKoddal {
 
+    private static Scanner sc = new Scanner(System.in);
+    private static int szamlalo;
+
     public static void main(String[] args) {
-        KodBekeres();
-        KodEllenorzes();
+        int kapottPin = KodBekeres();
+        KodEllenorzes(kapottPin);
+
+    }
+
+    private static int KodBekeres() {
+        System.out.print("Kérem adja meg a pinkódot: ");
+        int pin = sc.nextInt();
+        System.out.println("Kód elmentve!");
+        return pin;
+    }
+
+    private static void KodEllenorzes(int kapottPin) {
+        szamlalo = 1;
+        int kod;
+
+        do {
+            System.out.printf("Kérem a belépési kódot(%d/3):", szamlalo);
+            kod = sc.nextInt();
+            System.out.println("");
+            szamlalo++;
+
+        } while (kod != kapottPin && szamlalo <= 3);
         
+        if (szamlalo < 4) {
+            System.out.println("Rendben sikeres belépés!");
+        }else{
+            System.out.println("Belépés megtagadva!");
+        }
     }
 
-    private static void KodBekeres() {
-    }
-
-    private static void KodEllenorzes() {
-    }
-    
 }
